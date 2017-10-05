c65368c62a184c490553eca953e83fec5724e47a
Karis Tenneson <karistenneson@gmail.com>
2017-10-05 15:09:20 -0600

    adding accuracy assessment instructions

--- AccuracyAssessment/Script/MergeData.R
+++ AccuracyAssessment/Script/MergeData.R
@@ -1,8 +1,23 @@
-## Pull in the CEO mapathon files.
-files = list.files(path = '.\\AccuracyAssessment\\Data\\CountryFiles', pattern="*.csv")
+WorkingDirectory <-'\\\\166.2.126.227\\International_Programs\\Mekong 2016\\githubAccount\\LandCoverMonitoring\\AccuracyAssessment'
 
-## Merge files into one csv.
-myfiles = do.call(rbind, lapply(files, function(x) read.csv(x, stringsAsFactors = FALSE)))
+setwd(WorkingDirectory)
+
+AllCountryfiles <- list.files(path = '.\\Data\\CountryFiles', pattern = ".csv", full.names = FALSE)
+AllData <- NULL
+
+for (i in 1:length(AllCountryfiles)){
+  dataTemp <- read.csv(paste0('.\\Data\\CountryFiles\\',AllCountryfiles[i]))
+  dataTemp$Country<-substr(AllCountryfiles[i], 11, nchar(AllCountryfiles[i])-15)
+  
+  if (i == 1){
+    AllData<- dataTemp
+  }
+  
+  AllData <-rbind(AllData, dataTemp)
+}
+
+head(AllData)
+unique(AllData$Country)
 
 ## Change column names before importing into EE.
 colnames(myfiles)<-c("PLOTID", "longitude", "latitude", "SIZEM", "SHAPE", 
@@ -11,7 +26,8 @@ colnames(myfiles)<-c("PLOTID", "longitude", "latitude", "SIZEM", "SHAPE",
                      "MUDFLATBEACH", "BARRENOTHER", "TREEPLANTATIONORCHARD", 
                      "TREEMANGROVE", "TREEOTHER", "SHRUB", "GRASS", "CROP", 
                      "AQUACULTUREPOND", "AQUATICVEGOTHER", "WATER", 
-                     "SNOWICE", "UNKNOWN", "OTHER")
+                     "SNOWICE", "UNKNOWN", "OTHER", "Country")
 
-## Export one csv.
-write.csv(myfiles, 'allPoints.csv', row.names = F)
+
+## Export as csv.
+write.csv(AllData, '.\\Data\\ceo-rlcms-Mekong.csv', row.names = F)
